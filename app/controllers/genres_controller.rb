a150fa9fea787d3b81c061e6be051232e1621716
Ali Shah <ali1195shah@gmail.com>
2019-10-15 20:23:30 +0000

    Automatically backed up by Learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,27 @@
+class GenresController < ApplicationController
+
+  def index
+    
+  end
+  
+  def show 
+    
+  end
+  
+  def edit 
+    
+  end
+  
+  def new 
+    
+  end 
+  
+  def create
+    
+  end 
+  
+  def update
+    
+  end
+  
+end 
