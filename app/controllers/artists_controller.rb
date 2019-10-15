818584e31030b6a7a7779e351a146fc3b8b7e35b
Ali Shah <ali1195shah@gmail.com>
2019-10-15 20:21:29 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,1 +1,27 @@
-class Atrists
+class AtristsController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def show 
+    
+  end
+  
+  def edit 
+    
+  end
+  
+  def new 
+    
+  end 
+  
+  def create
+    
+  end 
+  
+  def update
+    
+  end
+  
+end
